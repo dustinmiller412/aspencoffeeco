92f8c0736d3ad9d83199d444c8c652470100a0dc
Dustin Miller <dustinrobertmiller412@gmail.com>
2026-07-07 14:42:42 -0400

    Fixing the subscription confirmation redirect

--- app/routes/customer_identity.confirmations.$token.jsx
+++ app/routes/customer_identity.confirmations.$token.jsx
@@ -1,5 +1,11 @@
 import {redirect} from 'react-router';
 
-export function loader() {
+export async function loader({params}) {
+  try {
+    await fetch(
+      `https://aspencoffeeco.myshopify.com/customer_identity/confirmations/${params.token}`,
+    );
+  } catch (_) {}
+
   return redirect('/?subscribed=true');
 }
